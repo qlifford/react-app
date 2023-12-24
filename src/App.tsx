bba2d2dbc4b6b5656fd9ceeae45fb1f8031fa9cb
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-12-24 07:54:14 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./message";
+
+function App() {
+  return <div className="">
+    <Message />
+  </div>;
+}
+export default App;
---
+++ src/message.tsx
@@ -0,0 +1,6 @@
+function Message(){
+
+    return <h1>Hey cliff, how are you today</h1>;
+}
+
+export default Message;
